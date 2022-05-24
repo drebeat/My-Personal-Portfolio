18d1a9fe042687ed95f925d6fe28d3a38a7ca286
drebeat <holamiderefined@gmail.com>
2022-05-24 14:27:34 +0100

    Added new changes

---
+++ src/components/about/About.jsx
@@ -0,0 +1,30 @@
+import './about.css'
+import ME from '../../assets/me-about.jpg'
+const About = () => {
+    return ( 
+        <section id="about">
+           <h5>Get To Know</h5>
+           <h2>About Me</h2>
+
+           <div className='container about__container'>
+               <div className="about__nme">
+                   <div className="about__me-image">
+                       <img src={ME} alt="About Image" />
+                   </div>
+               </div>
+               <div className="about__content">
+                   <div className="about__cards">
+                       <article className='about__card'>
+                            <h5>Experience</h5>
+                            <small>1+ Years Working</small>
+                       </article>
+                   </div>
+               </div>
+           </div>
+
+
+        </section>
+     );
+}
+ 
+export default About;
